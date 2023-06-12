d282f2175fbdd3d2cd412091f5403b0dd3549112
KingKiarie <kiarie7mbugua@gmail.com>
2023-06-12 11:28:47 +0300

    Modified html and js pages

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
- const parent = document.querySelector('.parent-list')
+const products = document.querySelector('.products')
+const parent = document.querySelector('.parent-list')
+const form= document.querySelector('.form')
+let searchParam =
+
+
  parent.addEventListener('click', function(event){
 
     const sublist= event.target.querySelector('.sublist')
@@ -6,6 +11,39 @@
        sublist.classList.toggle('hidden')
     }
 })
+
+form.addEventListener('submit', async (e)=>{
+    e.preventDefault()
+    const input = form.querySelector('.placeholder')
+    searchParam=input.value
+    products.innerHTML=''
+    console.log(searchParam)
+
+    try{
+        searchProducts(searchParam)
+    }catch(error) {
+        console.log(error)
+    }
+
+
+})
+
+const searchProducts =async (products)=>{
+    const response= await fetch(`https://dummyjson.com/products/search?q=${product}`)
+    const data = await response.json()
+    data.products.forEach((item)=>{
+        console.log(item)
+        const container = document.createElement('div')
+        container.classList.add('containerProduct')
+        container.innerHTML=`
+        <div class="product-image"><img src="${item.thumbnail}"/></div>
+        <h2>${item.title}</h2>
+        <h3>ksh. ${item.price}</h3>
+        <button>Add to cart</button>
+        `
+        product.appendChild(container)
+    })
+}
 const fetchData = async()=>{
     const response = await fetch('https://fakestoreapi.com/products')
     const data = await response.json()
