d9f93f46ff9fefe792a86ceb9652d9b27e94ed45
KingKiarie <kiarie7mbugua@gmail.com>
2023-06-13 08:27:46 +0300

    cleared footer

--- index.js
+++ index.js
@@ -12,6 +12,27 @@ let searchParam =
     }
 })
 
+const fetchData = async()=>{
+    const response = await fetch('https://fakestoreapi.com/products')
+    const data = await response.json()
+    console.log(data)
+
+    data.results.forEach((product)=>{
+        const container=document.createElement('div')
+        container.classList.add('container')
+
+        container.innerHTML=`
+        <div><img class="cnt-img" src="${product.image}"</div>
+        <h2>${product.title}</h2>
+        <h3>${product.description}</h3>
+        <h3>${product.category}</h3>
+        <h3>${product.rating}</h3>
+        <h3>${product.price}</h3>
+        `
+        document.body.appendChild(container)
+    })
+}
+
 form.addEventListener('submit', async (e)=>{
     e.preventDefault()
     const input = form.querySelector('.placeholder')
@@ -41,27 +62,8 @@ const searchProducts =async (products)=>{
         <h3>ksh. ${item.price}</h3>
         <button>Add to cart</button>
         `
-        product.appendChild(container)
+        product.appendChild(conta)
     })
 }
-const fetchData = async()=>{
-    const response = await fetch('https://fakestoreapi.com/products')
-    const data = await response.json()
-    console.log(data)
 
-    data.results.forEach((product)=>{
-        const container=document.createElement('div')
-        container.classList.add('container')
-
-        container.innerHTML=`
-        <div><img class="cnt-img" src="${product.image}"</div>
-        <h2>${product.title}</h2>
-        <h3>${product.description}</h3>
-        <h3>${product.category}</h3>
-        <h3>${product.rating}</h3>
-        <h3>${product.price}</h3>
-        `
-        document.body.appendChild(container)
-    })
-}
 fetchData()
