004ce7dca6fba0d741361674fff3f331232018bc
KingKiarie <kiarie7mbugua@gmail.com>
2023-06-15 13:58:55 +0300

    finished Signup pages

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const products = document.querySelector('.products')
 const parent = document.querySelector('.parent-list')
 const form= document.querySelector('.form')
-let searchParam =
+const product=document.querySelector('.products')
+let searchParam = 'phone'
+let cartItems = []
 
 
  parent.addEventListener('click', function(event){
@@ -12,32 +14,32 @@ let searchParam =
     }
 })
 
-const fetchData = async()=>{
-    const response = await fetch('https://fakestoreapi.com/products')
-    const data = await response.json()
-    console.log(data)
+// // const fetchData = async()=>{
+//     const response = await fetch('https://fakestoreapi.com/products')
+//     const data = await response.json()
+//     console.log(data)
 
-    data.results.forEach((product)=>{
-        const container=document.createElement('div')
-        container.classList.add('container')
+//     data.forEach((product)=>{
+//         const container=document.createElement('div')
+//         container.classList.add('container')
 
-        container.innerHTML=`
-        <div><img class="cnt-img" src="${product.image}"</div>
-        <h2>${product.title}</h2>
-        <h3>${product.description}</h3>
-        <h3>${product.category}</h3>
-        <h3>${product.rating}</h3>
-        <h3>${product.price}</h3>
-        `
-        document.body.appendChild(container)
-    })
-}
+//         container.innerHTML=`
+//         <div><img class="cnt-img" src="${product.image}"</div>
+//         <h2>${product.title}</h2>
+//         <h3>${product.description}</h3>
+//         <h3>${product.category}</h3>
+//         <h3>${product.rating}</h3>
+//         <h3>${product.price}</h3>
+//         `
+//         document.body.appendChild(container)
+//     })
+// }
 
 form.addEventListener('submit', async (e)=>{
     e.preventDefault()
     const input = form.querySelector('.placeholder')
     searchParam=input.value
-    products.innerHTML=''
+    product.innerHTML=''
     console.log(searchParam)
 
     try{
@@ -50,7 +52,7 @@ form.addEventListener('submit', async (e)=>{
 })
 
 const searchProducts =async (products)=>{
-    const response= await fetch(`https://dummyjson.com/products/search?q=${product}`)
+    const response= await fetch(`https://dummyjson.com/products/search?q=${products}`)
     const data = await response.json()
     data.products.forEach((item)=>{
         console.log(item)
@@ -59,10 +61,11 @@ const searchProducts =async (products)=>{
         container.innerHTML=`
         <div class="product-image"><img src="${item.thumbnail}"/></div>
         <h2>${item.title}</h2>
-        <h3>ksh. ${item.price}</h3>
-        <button>Add to cart</button>
+        <h3>$. ${item.price}</h3>
+        <p>Quantity: <input type="number" value="1"></p>
+        <button class="cartbutton">Add to cart</button>
         `
-        product.appendChild(conta)
+        product.appendChild(container)
     })
 }
 
